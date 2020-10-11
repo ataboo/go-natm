a14f0072db109df41c08930fb99153a414cc3231
Alex Raboud <doubar2001@gmail.com>
2020-10-10 20:04:45 -0600

    adding comment buttons

--- frontend/src/components/main/routes/project/column/taskdetailmodal/taskcomment/taskcomment.tsx
+++ frontend/src/components/main/routes/project/column/taskdetailmodal/taskcomment/taskcomment.tsx
@@ -1,7 +1,7 @@
 import { DateTime } from 'luxon';
-import React from 'react';
+import React, { FormEvent, useState } from 'react';
 import { Button, Card } from 'react-bootstrap';
-import { Trash } from 'react-bootstrap-icons';
+import { PencilSquare, Trash } from 'react-bootstrap-icons';
 import { CommentRead } from '../../../../../../../models/comment';
 import { User } from '../../../../../../../models/user';
 
@@ -11,22 +11,32 @@ type TaskCommentProps = {
     currentUser: User;
 }
 
-export const TaskComment = ({comment, deleteComment, currentUser}: TaskCommentProps) => {
-    const renderTrashButton = () => {
+export const TaskComment = ({comment, deleteComment, editComment, currentUser}: TaskCommentProps) => {
+    const [editing, setEditing] = useState(false);
+    
+    const renderButtons = () => {
         if (currentUser.id === comment.author.id) {
-            return <Button type="button" className="btn-light p-1" onClick={() => deleteComment(comment.id)}><Trash/></Button>
+            return (<div>
+                <Button type="button" className="btn-light p-1" onClick={() => setEditing(true)}><PencilSquare/></Button>
+                <Button type="button" className="btn-light p-1" onClick={() => deleteComment(comment.id)}><Trash/></Button>
+            </div>);
         }
     }
     
+    const onEditFormSubmit = (e: FormEvent) => {
+
+    }
     
     return (
     <Card className="mb-2">
         <Card.Header className="d-flex justify-content-between align-items-center py-1">
                 <span>{comment.author.name}@{DateTime.fromISO(comment.createdAt).toLocaleString(DateTime.DATETIME_SHORT)}</span>
-                {renderTrashButton()}
+                {renderButtons()}
         </Card.Header>
         <Card.Body>
-            <Card.Text>{comment.message}</Card.Text>
+            <Card.Text>{{if(editing) {
+                    return (<span>{comment.message}</span>);
+                }}}</Card.Text>
         </Card.Body>
     </Card>);
 };
